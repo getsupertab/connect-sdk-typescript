4945a9ad26dc666960c7e63db56c0393191b8a80
Rana <ranaelgarem781994@gmail.com>
2025-10-28 13:39:23 +0300

    config update

--- demos/fastly/src/config.js
+++ demos/fastly/src/config.js
@@ -3,7 +3,10 @@
  * You should replace these values with your own credentials.
  */
 export const CONFIG = {
-  MERCHANT_SYSTEM_URN: "urn:stc:merchant:system:test-id",
-  MERCHANT_API_KEY: "stc_live_cyysbuP9nXQmQkgn-5vrhUr4lEWm_702",
-  BASE_URL: "http://localhost:8000"
+  MERCHANT_SYSTEM_URN: "urn:stc:merchant:system:bc2602b5-439f-463c-a5e2-fa9cebec472a",
+  MERCHANT_API_KEY: "stc_live_6H4ABI82cIQjKwztfS9j80qH9xmKGKCU",
+  BASE_URL: "http://127.0.0.1:8000"
+  // MERCHANT_SYSTEM_URN: "urn:stc:merchant:system:745da447-d216-4196-af3b-503e3ef2c4b3",
+  // MERCHANT_API_KEY: "stc_live_oo_sEVy6Q8rbjfxUcR-aPMp211uQveA0",
+  // BASE_URL: "https://api-connect.sbx.supertab.co",
 };
